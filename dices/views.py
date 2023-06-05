3d626b5051de809b7249ac3f5e1e347b220988af
amiv.80 <amir.r.v138@gmail.com>
2023-06-05 22:15:49 +0330

    Remove some useless lines, Remove Finish button and add Roll Again button

--- dices/views.py
+++ dices/views.py
@@ -5,6 +5,14 @@ def roll_dice_view(request):
     if request.method == 'POST':
         sides = request.POST.get('sides')
         num_dice = request.POST.get('num_dice')
+        roll_again = request.POST.get('roll_again')
+
+        if roll_again:
+            sides = request.session.get('sides')
+            num_dice = request.session.get('num_dice')
+        else:
+            request.session['sides'] = sides
+            request.session['num_dice'] = num_dice
 
         if not sides or not num_dice:
             return render(request, 'dices/roll_dice.html', {'error': 'Please enter a valid input.'})
@@ -16,13 +24,12 @@ def roll_dice_view(request):
             return render(request, 'dices/roll_dice.html', {'error': 'Please enter valid integers.'})
 
         results = []
-        while True:
-            current_round = []
-            for i in range(1, num_dice + 1):
-                dice_result = random.randint(1, sides)
-                current_round.append(dice_result)
-            results.append(current_round)
+        current_round = []
+        for i in range(1, num_dice + 1):
+            dice_result = random.randint(1, sides)
+            current_round.append(dice_result)
+        results.append(current_round)
 
-            return render(request, 'dices/roll_dice.html', {'results': results})
+        return render(request, 'dices/roll_dice.html', {'results': results})
 
     return render(request, 'dices/roll_dice.html')
