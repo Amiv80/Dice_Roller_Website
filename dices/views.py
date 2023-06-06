b92e0f82230631b15533c8d5064643e46fa633e7
amiv.80 <amir.r.v138@gmail.com>
2023-06-06 09:30:12 +0330

    Removed the unnecessary assignment of sides and num_dice from the roll_again

--- dices/views.py
+++ dices/views.py
@@ -3,14 +3,14 @@ import random
 
 def roll_dice_view(request):
     if request.method == 'POST':
-        sides = request.POST.get('sides')
-        num_dice = request.POST.get('num_dice')
         roll_again = request.POST.get('roll_again')
 
         if roll_again:
             sides = request.session.get('sides')
             num_dice = request.session.get('num_dice')
         else:
+            sides = request.POST.get('sides')
+            num_dice = request.POST.get('num_dice')
             request.session['sides'] = sides
             request.session['num_dice'] = num_dice
 
@@ -24,12 +24,10 @@ def roll_dice_view(request):
             return render(request, 'dices/roll_dice.html', {'error': 'Please enter valid integers.'})
 
         results = []
-        current_round = []
-        for i in range(1, num_dice + 1):
+        for _ in range(num_dice):
             dice_result = random.randint(1, sides)
-            current_round.append(dice_result)
-        results.append(current_round)
+            results.append(dice_result)
 
-        return render(request, 'dices/roll_dice.html', {'results': results})
+        return render(request, 'dices/roll_dice.html', {'results': [results]})
 
     return render(request, 'dices/roll_dice.html')
