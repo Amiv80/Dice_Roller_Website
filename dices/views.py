0217a5f04ad7447c3b2451d94d296ee052c5057c
Amiv80 <amir.r.v138@gmail.com>
2023-09-10 12:11:26 +0330

    run Black Formatter

--- dices/views.py
+++ dices/views.py
@@ -1,33 +1,48 @@
 from django.shortcuts import render
 import random
+from collections import Counter
+
 
 def roll_dice_view(request):
-    if request.method == 'POST':
-        roll_again = request.POST.get('roll_again')
+    if request.method == "POST":
+        roll_again = request.POST.get("roll_again")
 
         if roll_again:
-            sides = request.session.get('sides')
-            num_dice = request.session.get('num_dice')
+            sides = request.session.get("sides")
+            num_dice = request.session.get("num_dice")
         else:
-            sides = request.POST.get('sides')
-            num_dice = request.POST.get('num_dice')
-            request.session['sides'] = sides
-            request.session['num_dice'] = num_dice
+            sides = request.POST.get("sides")
+            num_dice = request.POST.get("num_dice")
+            request.session["sides"] = sides
+            request.session["num_dice"] = num_dice
 
         if not sides or not num_dice:
-            return render(request, 'dices/roll_dice.html', {'error': 'Please enter a valid input.'})
+            return render(
+                request,
+                "dices/roll_dice.html",
+                {"error": "Please enter a valid input."},
+            )
 
         try:
             sides = int(sides)
             num_dice = int(num_dice)
         except ValueError:
-            return render(request, 'dices/roll_dice.html', {'error': 'Please enter valid integers.'})
+            return render(
+                request,
+                "dices/roll_dice.html",
+                {"error": "Please enter valid integers."},
+            )
 
         results = []
         for _ in range(num_dice):
             dice_result = random.randint(1, sides)
             results.append(dice_result)
 
-        return render(request, 'dices/roll_dice.html', {'results': [results]})
+        number_counts = dict(Counter(results))
+        return render(
+            request,
+            "dices/roll_dice.html",
+            {"results": results, "number_counts": number_counts},
+        )
 
-    return render(request, 'dices/roll_dice.html')
+    return render(request, "dices/roll_dice.html")
