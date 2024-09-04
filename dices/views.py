ded4e0efcc28da8e4906e55cff3987bd168eae57
Amiv80 <amir.r.v138@gmail.com>
2024-09-04 21:30:03 +0330

    change roll_dice html & fix views.py

--- dices/views.py
+++ dices/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import DiceForm
 import random
 
 
 def roll_dice_view(request):
-    # Check if the request method is POST
     if request.method == "POST":
         form = DiceForm(request.POST)
-        # Check if the form is valid
         if form.is_valid():
-            # If valid, get the number of sides and dice from the form data
             sides = form.cleaned_data["sides"]
             num_dice = form.cleaned_data["num_dice"]
-            # Generate a list of random numbers, each between 1 and the number of sides
+
+            if sides < 1 or num_dice < 1:
+                messages.error(request, "Number of sides and dice must be at least 1.")
+                return render(request, "dices/roll_dice.html", {"form": form})
+
             results = [random.randint(1, sides) for _ in range(num_dice)]
-            # Render the template with the form and results
             return render(
                 request,
                 "dices/roll_dice.html",
@@ -25,8 +26,9 @@ def roll_dice_view(request):
                     "num_dice": num_dice,
                 },
             )
+        else:
+            messages.error(request, "Please correct the errors below.")
     else:
-        # If the request method is not POST, create a new form
         form = DiceForm()
-    # Render the template with the form
+
     return render(request, "dices/roll_dice.html", {"form": form})
